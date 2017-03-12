c674fdb1eff6ce02bc775c8ad90eb6e71e2b5e95
Zoltan Bodi <zbodi74@gmail.com>
2017-03-12 20:29:48 +0100

    Assignment solution

---
+++ plot1.R
@@ -0,0 +1,24 @@
+library(lubridate)
+
+zipfn<-"househol_power_consumption.zip"
+if (!file.exists(zipfn)) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile=zipfn)
+}
+raw<-read.table(unz(zipfn, "household_power_consumption.txt"), 
+                sep=";", 
+                header = TRUE,
+                na.strings = "?",
+                stringsAsFactors = FALSE)
+
+raw$date<-dmy(raw$Date)
+raw$datetime<-dmy_hms(paste(raw$Date, raw$Time))
+raw$Date<-NULL
+raw$Time<-NULL
+
+plotdata<-raw[raw$date %within% interval(ymd("20070201"),ymd("20070202")),]
+
+png("plot1.png")
+hist(plotdata$Global_active_power,
+     main = "Global Active Power", xlab = "Global Active Power (kilowatts)",
+     col = "red")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,41 @@
+library(lubridate)
+
+zipfn<-"househol_power_consumption.zip"
+if (!file.exists(zipfn)) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile=zipfn)
+}
+raw<-read.table(unz(zipfn, "household_power_consumption.txt"), 
+           sep=";", 
+           header = TRUE,
+           na.strings = "?",
+           stringsAsFactors = FALSE)
+
+raw$date<-dmy(raw$Date)
+raw$datetime<-dmy_hms(paste(raw$Date, raw$Time))
+raw$Date<-NULL
+raw$Time<-NULL
+
+plotdata<-raw[raw$date %within% interval(ymd("20070201"),ymd("20070202")),]
+
+par(mfrow=c(2,2))
+png("plot4.png")
+
+#1
+plot(x=plotdata$datetime, y=plotdata$Global_active_power, type="l",
+     ylab = "Global Active Power",xlab=NA)
+
+#2
+plot(x=plotdata$datetime, y=plotdata$Voltage, type="l", col="black", xlab="datetime")
+
+#3
+plot(x=plotdata$datetime, y=plotdata$Sub_metering_1, 
+     type="l", col="black",
+     ylab = "Energy sub metering", xlab=NA)
+lines(x=plotdata$datetime,y=plotdata$Sub_metering_2,type="l", col="red")
+lines(x=plotdata$datetime,y=plotdata$Sub_metering_3,type="l", col="blue")
+legend(legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), x="topright", col=c("black","red","blue"), lty=1)
+
+#4
+plot(x=plotdata$datetime, y=plotdata$Global_reactive_power, type="l",xlab="datetime")
+
+dev.off()
